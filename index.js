c641df96af05239c92321ba356a2e2f85df18471
abhishek singh <abhishek.singh@instarem.com>
2020-01-07 20:31:12 +0530

    In progress

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
-console.log("Hello, world!");
-var co = require("co");
-var prompt = require("co-prompt");
-var program = require("commander");
+console.log("Hi this is robo!!");
+let co = require("co");
+let prompt = require("co-prompt");
+let program = require("commander");
+
+let areaUnit = 1
+let area = {
+    size: 5 & areaUnit
+}
+
+let VALID_COMMANDS =  {
+  PLACE : "PLACE", 
+  MOVE: "MOVE",
+  LEFT :"LEFT",
+  RIGHT: "RIGHT",
+  REPORT: "REPORT"
+}
 
 program
   .arguments("<file>")
@@ -11,22 +24,27 @@ program
   .action(function(file) {
     co(function*() {
       let robo = {
-        x: 0,
-        y: 0,
-        direction: "north"
       };
 
-      let posx = yield prompt("posx: ");
-      isNaN(parseInt(posx)) && robo.x = parseInt(posx);
+      let posx, posy, direction, command
 
-      let posy = yield prompt("posy: ");
-      isNaN(parseInt(posy)) robo.y = parseInt(posy);
+      posx = yield prompt("");
+      console.log(posx)
 
-      let direction = yield prompt("direction: ");
+
+
+      posx = parseInt(posx);
+      if( isNaN(posx)) robo.x = posx;
+
+      posy = yield prompt("posy: ");
+      posy = parseInt(posy);
+      if( isNaN(posy)) robo.y = posy;
+
+      direction = yield prompt("direction: ");
       robo.direction = direction;
 
       do {
-        let command = yield prompt("command: ");
+        command = yield prompt("command: ");
         if (command === "move") {
           if (robo.direction === "north") {
             robo.x += 1;
@@ -61,3 +79,24 @@ program
     });
   })
   .parse(process.argv);
+
+
+
+  function applyCommand(robo, command) {
+
+      let isPlaced = !!robo.x && !!robo.y && !!robo.f
+
+      if(command.includes(VALID_COMMANDS.PLACE)) {
+
+      }else if(isPlaced) {
+          //listen to all cammands
+      }else {
+        console.log("First place the robot")
+      }
+      
+      return {
+          x, 
+          y,
+          f
+      }
+  }
