28454c16848e51433a104ae2411e8b3df7240846
abhishek singh <abhishek.singh@instarem.com>
2020-01-09 19:52:28 +0530

    Styling and deploy

--- web/src/App.js
+++ web/src/App.js
@@ -10,21 +10,32 @@ let roboPlayArea = robo.setArea(areaUnit)
 
 console.log(roboPlayArea)
 
+let enteredComment = null
 function App() {
   const [myRobo, updateRobo] = useState({})
   const [update, toggleUpdate] = useState(false)
+  const [enteredCommand, setCommand] = useState(null)
   console.log(myRobo)
+
   return (
     <div>
       <form>
-        <input type={"text"} name="command" onBlur={e => {
-          updateRobo(robo.applyCommand(myRobo, e.target.value))
-          toggleUpdate(!update)
+        <input type={"text"} name="command" 
+         placeholder="Enter command"
+         onBlur={e => {
+          setCommand(e.target.value)
         }}
-          style={{ width: 200, height: 40 }}></input>
-        <input type="submit" value="Submit" onClick={event => {
+          style={{ width: 200, height: 30, margin:10 }}></input>
+        <input type="submit" value="Send Command" onClick={event => {
           event.preventDefault();
-        }}></input>
+          if(enteredCommand) {
+            updateRobo(robo.applyCommand(myRobo, enteredCommand))
+            toggleUpdate(!update)
+          }
+        }}
+        style={{ width: 200, height: 40 }}
+        ></input>
+        <span style={{fontSize:12,color:"grey"}}> Commands example: place 0,0,north | move| left| right</span>
       </form>
       <div style={{ marginLeft: roboPlayArea.size, position: 'relative', width: roboPlayArea.size + areaUnit, height: roboPlayArea.size + areaUnit, borderLeft: "1px solid black", borderBottom: "1px solid black" }}>
         {myRobo.placed && <div
